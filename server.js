77475c6474ef86bda28d1451f5fd95754e95a4a6
Mike Brevoort <mike@brevoort.com>
2017-07-18 13:07:06 -0700

    Clean up error handling

--- server.js
+++ server.js
@@ -12,30 +12,27 @@ app.get("/", (req, res) => {
 // Your service will get a post from Missions
 app.post("/", json(), function (req, res) {
   // Assign the input from Missions to a variable
-  var input = req.body.input[1].value
-  console.log('Input received: ' + input)
+  let input = req.body.input && req.body.input.find(i => i.name === 'movie')
+  if (!input) return res.status(412).send('movie input not found')  
+  console.log('Input received: ' + input.value)
   
   // Check if the verification token is valid
   var verify_token = req.get('x-verification');
-
-  if (verify_token === process.env.VERIFY_TOKEN) {
-    // Return something back to Missions as an output
-    res.json({
-      handled: true,
-      output: {
-        movie_string: input + ' is a great movie.'
-      }
-    }) 
-  } else {
-    // Return an error
+  if (verify_token !== process.env.VERIFY_TOKEN) {
     console.log('Verify token doesn\'t match.')
-    res.status(500).send({
-      error: 'Invalid token'
-    })
+    return res.status(403).send('Verification token does not match 🙅')
   }
+
+  // Return something back to Missions as an output
+  res.json({
+    handled: true,
+    output: {
+      movie_string: `${input.value} is a great movie.`
+    }
+  }) 
 })
 
 // Listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port)
+  console.log('Your app is 🎧 on port ' + listener.address().port)
 })
